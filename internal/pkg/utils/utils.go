bfb1a0ae1391118dcc2f62b41fe83a0e0c8d7f31
liuming <shallmin93203x>
2021-12-13 17:54:28 +0800

    项目搭建

--- internal/pkg/utils/utils.go
+++ internal/pkg/utils/utils.go
@@ -5,13 +5,8 @@ import (
 	"crypto/md5"
 	"crypto/rand"
 	"encoding/hex"
-	"encoding/json"
-	"io"
-	"io/ioutil"
 	"math/big"
-	"net/http"
 	"strconv"
-	"time"
 )
 
 var defaultLetters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
@@ -38,59 +33,4 @@ func RandomNumStr(digit int) string {
 		bf.WriteString(randomNumStr)
 	}
 	return bf.String()
-}
-
-// 发送POST请求
-// url：         请求地址
-// data：        POST请求提交的数据
-// contentType： 请求体格式，如：application/json
-// content：     请求放回的内容
-func Post(url string, data interface{}, AppKey string, Nonce string, CurTime string, CheckSum string) string {
-
-	// 超时时间：5秒
-	client := &http.Client{Timeout: 5 * time.Second}
-	jsonStr, _ := json.Marshal(data)
-	request, err2 := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-	if err2 != nil {
-		return ""
-	}
-	request.Header.Add("AppKey", AppKey)
-	request.Header.Add("Nonce", Nonce)
-	request.Header.Add("CurTime", CurTime)
-	request.Header.Add("CheckSum", CheckSum)
-	request.Header.Set("Content-Type", "application/json;charset=UTF-8")
-	resp, err := client.Do(request)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	result, _ := ioutil.ReadAll(resp.Body)
-	return string(result)
-}
-
-// 发送GET请求
-// url：         请求地址
-// response：    请求返回的内容
-func Get(url string) string {
-
-	// 超时时间：5秒
-	client := &http.Client{Timeout: 5 * time.Second}
-	resp, err := client.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	var buffer [512]byte
-	result := bytes.NewBuffer(nil)
-	for {
-		n, err := resp.Body.Read(buffer[0:])
-		result.Write(buffer[0:n])
-		if err != nil && err == io.EOF {
-			break
-		} else if err != nil {
-			panic(err)
-		}
-	}
-
-	return result.String()
 }
